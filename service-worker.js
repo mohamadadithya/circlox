3ee8aa5749e6e3ab8fb0f61d6273b019e46c8302
Mohamad Adithya <75943603+mohamadadithya@users.noreply.github.com>
2021-08-04 15:19:51 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,15 @@
 const CACHE_NAME = 'SW-001';
 const toCache = [
     '/',
-    'assets/css/main.css',
-    'assets/js/app.js',
-    'assets/fonts/Poppins-Light.ttf',
-    'assets/fonts/Poppins-Medium.ttf',
+    'assets/css/main.min.css',
+    'assets/css/all.min.css',
+    'assets/js/app.min.js',
     'assets/js/web.webmanifest',
     'assets/js/register.js',
+    'assets/fonts/Poppins-Light.ttf',
+    'assets/fonts/Poppins-Medium.ttf',
+    'assets/webfonts/fa-solid-900.woff',
+    'assets/webfonts/fa-solid-900.woff2',
     'assets/icons/icon.png',
 ];
 
